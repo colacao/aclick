8bf85059b16e57da2c4e4953d7b1b1bbdd380825
colacao <caoyueqq@163.com>
2012-12-25 20:10:24 +0800

    edit index.html

--- aclick.js
+++ aclick.js
@@ -7,7 +7,7 @@
                  * 初始化事件处理器 - this指向元素
                  * @param 附加的数据
                  * @param 事件类型命名空间
-                 * @param 回调函数
+                 * @param 回调函数111
                  */
                 setup: function (data, namespaces, eventHandle) {
                         var elem = this;
