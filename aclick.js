0fe085f9cedeffbc58d5ce3d5bcfcae1253f6a6c
colacao <caoyueqq@163.com>
2012-12-26 10:54:31 +0800

    edit js

--- aclick.js
+++ aclick.js
@@ -1,5 +1,5 @@
 (function ($) {
-        jQuery.event.special.aclick= {
+        jQuery.event.special.aclick = {
                 /**
                  * 初始化事件处理器 - this指向元素
                  * @param 附加的数据
@@ -8,8 +8,12 @@
                  */
                 setup: function (data, namespaces, eventHandle) {
                         var elem = this;
-                        $.event.add(elem, 'touchend', function (event) {
-                                $.event.trigger('input', null, this);          
+                        $.event.add(elem, 'touchend', function (e) {
+                                var el = e.target;
+                                if(jQuery.event.special.aclick.tag){
+                                        $.event.trigger('touchend', null, el);  
+                                }
+                                jQuery.event.special.aclick.tag = null;     
                         });
                 },
                 /**
@@ -19,8 +23,15 @@
                 teardown: function (namespaces) {
                         var elem = this;
                         $.event.remove(elem, 'touchend');
-                }
+                },
+                tag:null
         };
+        document.body.addEventListener('touchstart', function(e) {
+                jQuery.event.special.aclick.tag = e.target;
+        }, false);
+        document.body.addEventListener('touchmove', function(e) {
+                jQuery.event.special.aclick.tag = null;
+        }, false);
         $.fn.aclick = function (callback) {
                 return this.bind('input', callback);
         };
